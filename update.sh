22a4fc3f03fd1e23b8f4d64f0c5f29362c10abb0
Ahmed Alroky <zcool2017@gmail.com>
2023-03-26 14:20:30 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@
 result=$(curl -i -s -k -X $'GET' \
     -H $'Authorization: sso-key api:secret' -H $'Content-Type: application/json' -H $'User-Agent: PostmanRuntime/7.29.0' -H $'Accept: */*' -H $'Postman-Token: f85ab54d-fb32-4e2c-aff3-959cda3ccd66' -H $'Host: api.godaddy.com' -H $'Accept-Encoding: gzip, deflate' -H $'Connection: close' -H $'Content-Length: 116' \
     --data-binary $'[    {        "data": "10.10.10.10",        "name": "vm",        "ttl": 1800,        "type": "A"    }]' \
-    $'https://api.godaddy.com/v1/domains/alroky.com/records/A/vm')
+    $'https://api.godaddy.com/v1/domains/YOUR_DOMAIN/records/A/vm')
 
 #echo $result;
 
@@ -21,7 +21,7 @@ currentIp=$(echo $ret | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"|head -n 1|tr 
 
 if [ "$dnsIp" != "$currentIp" ];
  then
-curl -i -s -k -X $'PUT' -H $'Authorization: sso-key api:secret' -H $'Content-Type: application/json' -H $'User-Agent: PostmanRuntime/7.29.0' -H $'Accept: */*' -H $'Postman-Token: 58633b35-749b-4548-bf6c-170024973dbb' -H $'Host: api.godaddy.com' -H $'Accept-Encoding: gzip, deflate' -H $'Connection: close' --data-binary $'[    {        "data": "'$currentIp'",        "name": "vm",        "ttl": 1800,        "type": "A"    }]' $'https://api.godaddy.com/v1/domains/alroky.com/records/A/vm'
+curl -i -s -k -X $'PUT' -H $'Authorization: sso-key api:secret' -H $'Content-Type: application/json' -H $'User-Agent: PostmanRuntime/7.29.0' -H $'Accept: */*' -H $'Postman-Token: 58633b35-749b-4548-bf6c-170024973dbb' -H $'Host: api.godaddy.com' -H $'Accept-Encoding: gzip, deflate' -H $'Connection: close' --data-binary $'[    {        "data": "'$currentIp'",        "name": "vm",        "ttl": 1800,        "type": "A"    }]' $'https://api.godaddy.com/v1/domains/YOUR_DOMAIN/records/A/vm'
 fi
 
 currentTime=$(date +"%Y-%m-%d %H:%M:%S")
